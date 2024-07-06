6443689c082d9fb427381be9fad348ca9a8aa82b
Abhijit Shingote <abhijitshingote@Abhijits-MacBook-Pro.local>
2024-07-06 08:43:19 -0400

    .

--- sort.c
+++ sort.c
@@ -1,81 +1,61 @@
-// #include <stdio.h>
-
-// #define MAXLINE 5
-
-// int get_line(char s[],int lim);
-
-// int main() {
-//     char currentline[MAXLINE];
-//     int length;
-//     // printf("Hello World");
-//     while ((length=get_line(currentline,MAXLINE))>0)
-//     {
-//         printf("%s",currentline);
-//     }
-    
-// }
-
-
-// int get_line(char s[],int lim) {
-//     int i,c;
-//     for (i = 0; i < lim -1 && (c=getchar())!= EOF; ++i)
-//     {
-//         s[i]=c;
-//     }
-
-//     s[i]='\0';
-
-//     return i;
-    
-
-// }
-
 #include <stdio.h>
-#define MAXLINE 5
 
-int length(char s[], int lim);
-void copy(char to[], char from[]);
+#define MAXLINE 5
+#define BUFFER_SIZE 5000
+static char buffer[BUFFER_SIZE];
+static char *p=buffer;
+int get_line(char s[],int lim);
+void copy_line_to_buffer(char *point,char currentline[]);
+char *alloc(int len);
 
 int main() {
-    int len;
-    int max;
-    char line[MAXLINE];
-    char longest[MAXLINE];
-
-    max = 0;
-    while ((len = length(line, MAXLINE)) > 0) {
-        // if (len > max) {
-        //     max = len;
-        //     copy(longest, line);
-        // }
-        printf("%s", line);
-        printf("\n");
+    char currentline[MAXLINE];
+    int length;
+    char *point;
+    // printf("Hello World");
+    while ((length=get_line(currentline,MAXLINE))>0 && (point=alloc(length))!=0)
+    {
+        copy_line_to_buffer(point,currentline);
     }
-
-    // if (max > 0) {
-    //     printf("%s", longest);
-    // }
-
-    return 0;
+    printf("%s",buffer);
+    
 }
 
-int length(char s[], int lim) {
-    int c, i;
+void copy_line_to_buffer(char *point,char currentline[]) {
+    int i = 0;
+    while (currentline[i]!='\0')
+    {
+        *point=currentline[i];
+        i++;
+        point++;
+    }
+    
+    *point=currentline[i];
+    
+}
 
-    for (i = 0; i < lim - 1 && (c = getchar()) != EOF ; ++i) {
+
+char *alloc(int len) {
+    if ((buffer+BUFFER_SIZE) - (p+len) > 0) {
+        p+=len;
+        return p - len;
+    }
+    else {
+        return 0;
+    }
+}
+
+int get_line(char s[], int lim)
+{
+    int c ,i ;
+    for(i = 0; i < lim - 1 && (c = getchar()) != EOF && c!= '\n'; ++i)
         s[i] = c;
-    }
-
-
-    s[i] = '\0';
-    return i;
-}
-
-void copy(char to[], char from []) {
-    int i;
-
-    i = 0;
-    while ((to[i] = from[i]) != '\0') {
+    if(c == '\n'){
+        s[i] = c;
         ++i;
     }
+    s[i] = '\0';
+    return i;
+    
 }
+
